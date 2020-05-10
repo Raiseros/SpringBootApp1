e6837ffd6a73ce3887dfca0c09a2973ad53e1bd2
Raiseros <holyavenko.91@mail.ru>
2020-05-10 03:50:10 +0300

    hello

---
+++ src/main/java/ru/holyav/springbootapp/crudloginform/service/SecurityService.java
@@ -0,0 +1,11 @@
+package ru.holyav.springbootapp.crudloginform.service;/*
+package ru.holyav.springbootapp.crudloginform.service;
+
+public interface SecurityService {
+
+    String findLoggedInUsername();
+
+    void autoLogin(String firstName, String password);
+
+}
+*/
